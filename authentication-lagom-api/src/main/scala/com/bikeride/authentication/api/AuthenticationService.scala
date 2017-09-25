058021d2460aedb468de368a2ff2ce7ba90be5f3
Fernando Hackbart <fhackbart@gmail.com>
2017-09-25 21:51:54 +0200

    bikeride-lagom-122 Authentication: Create service API
    Enabled the endpoint

--- authentication-lagom-api/src/main/scala/com/bikeride/authentication/api/AuthenticationService.scala
+++ authentication-lagom-api/src/main/scala/com/bikeride/authentication/api/AuthenticationService.scala
@@ -15,7 +15,7 @@ trait AuthenticationService extends Service {
     import Service._
     named("authn")
       .withCalls(
-        //restCall(Method.POST,"/api/authn/biker", createBiker),
+        restCall(Method.POST,"/api/authn/biker", createBiker),
         restCall(Method.POST,"/api/authn/validate", validatePIN _),
         restCall(Method.POST,"/api/authn/getpin", generatePIN _)
       ).withAutoAcl(true)
