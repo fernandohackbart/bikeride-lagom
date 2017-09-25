56056e7162bdc4d5dbcc2abe80dfa29863879b5d
Fernando Hackbart <fhackbart@gmail.com>
2017-09-25 19:42:09 +0200

    bikeride-lagom-122 Authentication: Create service API

---
+++ authentication-lagom-api/src/main/scala/com/bikeride/authentication/api/AuthenticationService.scala
@@ -0,0 +1,42 @@
+package com.bikeride.authentication.api
+
+import play.api.libs.json.{Format, Json}
+import com.bikeride.biker.api.{BikerCreateRequest, BikerToken, BikerClient}
+import com.lightbend.lagom.scaladsl.api.{Service, ServiceCall}
+import com.lightbend.lagom.scaladsl.api.transport.Method
+
+trait BikerService extends Service {
+
+  def createBiker: ServiceCall[BikerCreateRequest, BikerToken]
+  def validatePIN: ServiceCall[ValidatePINRequest, BikerToken]
+  def generatePIN: ServiceCall[GeneratePINRequest, GeneratePINResponse]
+
+  override final def descriptor = {
+    import Service._
+    named("authn")
+      .withCalls(
+        restCall(Method.POST,"/api/authn/biker", createBiker),
+        restCall(Method.POST,"/api/authn/validate", validatePIN _),
+        restCall(Method.POST,"/api/authn/getpin", generatePIN _)
+      ).withAutoAcl(true)
+  }
+
+}
+
+case class ValidatePINRequest(clientID: BikerClient,
+                              pin: Int)
+object  ValidatePINRequest {
+  implicit val format: Format[ValidatePINRequest] = Json.format
+}
+
+case class GeneratePINRequest(clientID: BikerClient,
+                              email: Option[String] = None,
+                              mobile: Option[String] = None)
+object  GeneratePINRequest {
+  implicit val format: Format[GeneratePINRequest] = Json.format
+}
+
+case class GeneratePINResponse(message: String)
+object  GeneratePINResponse {
+  implicit val format: Format[GeneratePINResponse] = Json.format
+}
