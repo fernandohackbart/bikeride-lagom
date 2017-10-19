e24e244b64b38a016f4facdf5c7fcaeaa5b33005
Fernando Hackbart <fhackbart@gmail.com>
2017-10-19 21:16:42 +0200

    Raised the token expiration time to 10 days

--- utils/src/main/scala/com/bikeride/utils/security/JWTConstants.scala
+++ utils/src/main/scala/com/bikeride/utils/security/JWTConstants.scala
@@ -4,7 +4,7 @@ import pdi.jwt.JwtAlgorithm
 
 object JWTConstants {
   val secret = "2o9WVew2qIFKQqV]2g:TKnzOpIGXQ@pr-!osvOi=v|,CpVe/]7m:@nNWM|LN.GAlxeL.eIn*%oS$m=*A,3N@hyGwZ=yA&=%>]g!Zt@=jMj|)KL8[qzylS7z0hjgRhA~m:&wR=LBT-;8V"
-  val authExpiration = 300
+  val authExpiration = 36000
   val refreshExpiration = 86400
   val algorithm = JwtAlgorithm.HS512
 }
