2aab47a926ad5640d40e309422f5387857c3d0b2
Fernando Hackbart <fhackbart@gmail.com>
2017-11-05 18:44:49 +0100

    Kubernetes
    Variables in the application.conf

--- Docker/docker-clean-images.sh
+++ Docker/docker-clean-images.sh
@@ -2,6 +2,5 @@
 IMAGE_COUNT=`docker images |grep bikeride|grep impl |wc -l`
 if [ ${IMAGE_COUNT} -gt 0 ]
 then
-  IMAGES_TO_REMOVE= `docker images |grep bikeride|grep impl |awk '{print $3 " "}' |tr -d '\n'`
-  docker rmi ${IMAGES_TO_REMOVE}
+  docker rmi `docker images |grep bikeride|grep impl |awk '{print $3 " "}' |tr -d '\n'`
 fi
